366f2b65fc7f4dccba509a5348031a96593e2f6d
Jermaine Walters <walte1jj@gmail.com>
2022-02-25 16:34:07 -0500

    console.log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,22 @@
-window.alert("this is an alert! Javascript is running!");
+// input and capture robot's name
+
+window.prompt("What is your robot's name?");
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+
+console.log(10 + 10);
+
+console.log("Our robt's name is " + playerName);
+
+// this creates a function name "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+fight();
+// fight();
